8d46be72cecb6283f4597924573c145be57c93db
André Matthies <matthiez@gmail.com>
2020-09-04 14:28:33 +0200

    added SmsOptions helper

---
+++ src/SmsOptions.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Sms77\Api;
+
+abstract class SmsOptions
+{
+    const Debug = 'debug';
+    const Delay = 'delay';
+    const Details = 'details';
+    const Flash = 'flash';
+    const ForeignId = 'foreign_id';
+    const From = 'from';
+    const Json = 'json';
+    const Label = 'label';
+    const NoReload = 'no_reload';
+    const PerformanceTracking = 'performance_tracking';
+    const ReturnMsgId = 'return_msg_id';
+    const Text = 'text';
+    const To = 'to';
+    const Ttl = 'ttl';
+    const Udh = 'udh';
+    const Unicode = 'unicode';
+    const Utf8 = 'utf8';
+}
