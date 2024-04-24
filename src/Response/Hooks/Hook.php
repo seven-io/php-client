bf781f903cb06ed9cd99b286cbb1e1bcbc7878c7
André Matthies <matthiez@gmail.com>
2024-04-24 11:57:20 +0200

    add missing Hook properties

--- src/Response/Hooks/Hook.php
+++ src/Response/Hooks/Hook.php
@@ -2,38 +2,59 @@
 
 namespace Seven\Api\Response\Hooks;
 
-class Hook {
+class Hook
+{
     protected string $created;
+    protected bool $enabled;
+    protected ?string $eventFilter;
     protected string $eventType;
     protected int $id;
     protected string $requestMethod;
     protected string $targetUrl;
 
-    public function __construct(object $data) {
+    public function __construct(object $data)
+    {
         $this->created = $data->created;
+        $this->enabled = $data->enabled;
+        $this->eventFilter = $data->event_filter;
         $this->eventType = $data->event_type;
         $this->id = (int)$data->id;
         $this->requestMethod = $data->request_method;
         $this->targetUrl = $data->target_url;
     }
 
-    public function getCreated(): string {
+    public function getCreated(): string
+    {
         return $this->created;
     }
 
-    public function getEventType(): string {
+    public function getEventType(): string
+    {
         return $this->eventType;
     }
 
-    public function getId(): int {
+    public function getId(): int
+    {
         return $this->id;
     }
 
-    public function getRequestMethod(): string {
+    public function getRequestMethod(): string
+    {
         return $this->requestMethod;
     }
 
-    public function getTargetUrl(): string {
+    public function getTargetUrl(): string
+    {
         return $this->targetUrl;
+    }
+
+    public function getEventFilter(): ?string
+    {
+        return $this->eventFilter;
+    }
+
+    public function isEnabled(): bool
+    {
+        return $this->enabled;
     }
 }
